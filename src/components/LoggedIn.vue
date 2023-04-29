a9f3753782c4195a2580e2e51ae942c39c64c3bc
Joel Christensen <christensenjo14@gmail.com>
2023-04-29 13:48:42 -0600

    Track cards display centered with backgoround gradient using average color of album img

--- src/components/LoggedIn.vue
+++ src/components/LoggedIn.vue
@@ -1,21 +1,32 @@
 <template>
-  <div class="p-8 rounded-md">
-    <h1 class="font-black text-4xl pb-8 mb-6">Top Tracks</h1>    
+  <div class="p-8 rounded-md flex flex-col justify-center text-center items-center">
     
-    <div v-for="item in tracksToDisplay" :key="item.id">
-      <p>{{ item.name }}</p>
-      <img  
-        :src="item.album.images[0].url" 
-        :alt="item.name" 
-        class="w-1/4 h-1/4" 
-      />
+    <div v-if="tracksToDisplay.length > 0">
+      <h1 class="font-black text-4xl pb-8 mb-6">Top Tracks</h1>
+
+      <div v-for="item in tracksToDisplay" 
+        :key="item.id"
+        class="flex flex-row justify-around items-center my-4 py-6 rounded-lg max-w-md mx-auto"
+        :style="`background-image: linear-gradient(to bottom, rgb(${item.averageColor[0]},${item.averageColor[1]},${item.averageColor[2]}), #222326)`"
+
+      >
+        <a :href="item.external_urls.spotify" class="w-1/3 flex justify-center">
+          <img  
+            :src="item.album.images[0].url" 
+            :alt="item.name" 
+            class="w-fit items-center ml-8" 
+          />
+        </a>
+        <div class="w-1/2 flex flex-col items-center">
+          <p class="">{{ item.name }}</p>
+          <p>by</p><p v-for="artist in item.artists">{{ artist.name }}</p>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+
     </div>
 
-    <!-- <p>Access Token: {{ accessToken }}</p>
-    <p>Token Type: {{ tokenType }}</p>
-    <p>Expires In: {{ expiresIn }}</p>
-    <p>Refresh Token: {{ refreshToken }}</p>
-    <p>Scope: {{ scope }}</p> -->
   </div>
 </template>
 
@@ -25,6 +36,7 @@ import querystring from "querystring";
 import { client_secret } from "./secrets";
 import { useSpotifyStore } from "@/store/spotifyStore.js";
 import { onMounted, computed } from "vue";
+import ColorThief from "colorthief";
 
 const client_id = "8c889c344b474adcae45b280704239fe";
 const redirect_uri = "http://localhost:5173/loggedIn";
@@ -56,15 +68,21 @@ export default {
             "Authorization": `${spotifyStore.tokenType} ${spotifyStore.accessToken}`,
           },
         });
-        
+
         if (response.data) {
           spotifyStore.topTracks = response.data;
+
+          for (const item of spotifyStore.topTracks.items) {
+            item.averageColor = await getAverageColor(item.album.images[0].url);
+          }
+
           console.log(spotifyStore.topTracks.items);
         }
       } catch (error) {
         console.error("Error fetching top tracks:", error);
       }
     };
+
 
     const tracksToDisplay = computed (() => {
       if(spotifyStore.topTracks 
@@ -76,6 +94,28 @@ export default {
         return [];
       }      
     });
+
+    const getAverageColor = async (imageUrl) => {
+      return new Promise((resolve, reject) => {
+        const colorThief = new ColorThief();
+        const img = new Image();
+        img.crossOrigin = "Anonymous";
+        img.src = imageUrl;
+
+        img.onload = () => {
+          try {
+            const color = colorThief.getColor(img);
+            resolve(color);
+          } catch (error) {
+            reject(error);
+          }
+        };
+
+        img.onerror = () => {
+          reject(new Error("Failed to load image"));
+        };
+      });
+    };
 
     onMounted(async () => {
       if(window.location.href.split("?").length >= 2) {
