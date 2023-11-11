3cb23d311c09a91665678477dfe478715a583db0
Joel Christensen <christensenjo14@gmail.com>
2023-11-11 13:36:45 -0700

    Logout button works as intended

--- src/store/spotifyStore.js
+++ src/store/spotifyStore.js
@@ -16,17 +16,17 @@ export const useSpotifyStore = defineStore({
     logout(){
       // 1. Clear the cookie and relevant data in the local state/store
       document.cookie = "state=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-      spotifyStore.code = "";
-      spotifyStore.state = "";
-      spotifyStore.accessToken = "";
-      spotifyStore.tokenType = "";
-      spotifyStore.expiresIn = 0;
-      spotifyStore.refreshToken = "";
-      spotifyStore.scope = "";
-
+      this.code = "";
+      this.state = "";
+      this.accessToken = "";
+      this.tokenType = "";
+      this.expiresIn = 0;
+      this.refreshToken = "";
+      this.scope = "";
+    
       // 2. Show an alert explaining that they need to revoke access via Spotify
       alert("To fully log out, you need to revoke access to this app via your Spotify account settings. You will be redirected to the Spotify account apps management page. Find joelbc-spotify on that page and click the 'Remove Access' button.");
-
+    
       // 3. Redirect the user to the Spotify account apps management page
       window.location.href = "https://www.spotify.com/us/account/apps/";
     },
