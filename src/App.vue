444b64912d0fbbf78b9704a2f7acfd1cac1b4075
Joel Christensen <christensenjo14@gmail.com>
2023-11-11 13:28:07 -0700

    Fix logout button positioning

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="flex flex-col min-h-screen">
-    <div class="flex items-center gap-6">
-      <img src="./assets/logo.png" class="w-20 p-2">
-      <h1 class="font-black text-4xl text-secondary">joelbc-spotify</h1>
-      <router-link to="/" class="ml-6 font-semibold text-xl" active-class="active-link">Home</router-link>
-      <router-link to="/my-music" class="ml-6 font-semibold text-xl" active-class="active-link">My Music</router-link>
-      <button v-if="pathname === '/my-music'" @click="logout" class="mt-4 bg-white hover:bg-secondary hover:text-white text-secondary font-bold py-2 px-4 rounded">
+    <div class="flex items-center justify-between">
+      <div class="flex items-center gap-6">
+        <img src="./assets/logo.png" class="w-20 p-2">
+        <h1 class="font-black text-4xl text-secondary">joelbc-spotify</h1>
+        <router-link to="/" class="ml-6 font-semibold text-xl" active-class="active-link">Home</router-link>
+        <router-link to="/my-music" class="ml-6 font-semibold text-xl" active-class="active-link">My Music</router-link>
+      </div>
+      <!-- Logout button aligned to the right -->
+      <button v-if="pathname === '/my-music'" @click="logout" class="bg-white hover:bg-secondary hover:text-white text-secondary font-bold py-2 px-4 rounded">
         Logout
       </button>
     </div>
